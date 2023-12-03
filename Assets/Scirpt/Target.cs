58a8627c433cbf3d0887bb7aee182199a958cf5d
CongSangDao <sangdaovnb2002@gmail.com>
2023-12-02 22:55:18 -0800

    Lesson D

--- Assets/Scirpt/Target.cs
+++ Assets/Scirpt/Target.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Target : MonoBehaviour
 {
@@ -58,8 +57,5 @@ public class Target : MonoBehaviour
         }
     }
 
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+   
 }
